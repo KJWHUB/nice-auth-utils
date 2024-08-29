2c1bd77f620820f86cf67822dabd40da98a4a660
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-08-29 18:47:23 +0900

    chore: update eslint.config.mjs to ignore "./docs/cache"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "./docs/cache"] },
   {
     extends: [
       eslintJs.configs.recommended,
