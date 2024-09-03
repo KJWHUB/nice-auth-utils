d085d5cf2882624eb96ddc2d2045804141b1f7c8
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-09-03 23:54:21 +0900

    chore: update eslint.config.mjs to ignore cache folder and update eslint-config-prettier dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,15 @@
 import eslintJs from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import prettierConfig from "eslint-config-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
-  { ignores: ["dist", "./docs/cache"] },
+  { ignores: ["dist", "**/cache"] },
   {
     extends: [
       eslintJs.configs.recommended,
       ...tseslint.configs.recommended,
-      prettierConfig,
+      eslintConfigPrettier,
     ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
